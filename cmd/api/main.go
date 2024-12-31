84a7a1d2560520e7316ca96d2b48dc2951761bde
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2024-12-31 20:39:06 +0630

    feat : add graceful shutdown for http server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,9 +6,6 @@ import (
 	"eillion/movie_api/internal/data"
 	"eillion/movie_api/internal/jsonlog"
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -75,24 +72,11 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	err = app.serve()
+
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
-
-	logger.PrintInfo("starting %s server on %s", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-
-	err = srv.ListenAndServe()
-
-	logger.PrintFatal(err, nil)
-
 }
 
 func openDB(cfg config) (*sql.DB, error) {
