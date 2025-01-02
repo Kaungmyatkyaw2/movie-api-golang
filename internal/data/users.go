08691592bdab27ea33f8cd8e0cf560a11a1d49e4
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2025-01-02 23:43:33 +0630

    feat:setup user model and database

---
+++ internal/data/users.go
@@ -0,0 +1,52 @@
+package data
+
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+func (p *password) Set(plainTextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plainTextPassword), 12)
+
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plainTextPassword
+	p.hash = hash
+
+	return nil
+}
+
+func (p *password) Matches(plainTextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plainTextPassword))
+
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+
+		}
+	}
+
+	return true, nil
+}
