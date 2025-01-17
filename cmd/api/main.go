626276dd62fc075be7074f30241bc7f965d628a3
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2025-01-17 15:53:16 +0630

    feat : enable cors manually with runtime configuration origins

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"eillion/movie_api/internal/mailer"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -69,6 +73,12 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "74644856878fe8", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "2eda21f9e2176d", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.eillion.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
