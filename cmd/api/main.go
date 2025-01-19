e1a0d261a5eab65db929d60317ab6b4223a89fbb
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2025-01-19 11:54:09 +0630

    feat : create make file for automation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -60,7 +60,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Enviroment")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "Postgresql DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "Postgresql DSN")
 
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
